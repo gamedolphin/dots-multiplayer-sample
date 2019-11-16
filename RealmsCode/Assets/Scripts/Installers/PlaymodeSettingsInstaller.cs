37683d565972f40ee04da5ee1248faad2ea80eca
Sandeep Nambiar <sandeep.turing@gmail.com>
2019-11-16 23:41:29 +0530

    Setup initial project

---
+++ RealmsCode/Assets/Scripts/Installers/PlaymodeSettingsInstaller.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using Zenject;
+
+[CreateAssetMenu(fileName = "PlaymodeSettingsInstaller", menuName = "Installers/PlaymodeSettingsInstaller")]
+public class PlaymodeSettingsInstaller : ScriptableObjectInstaller<PlaymodeSettingsInstaller>
+{
+    public override void InstallBindings()
+    {
+    }
+}
